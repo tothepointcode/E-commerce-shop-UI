01365879e5a3ba7610cee0c3e9852c824c415472
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-05-12 17:51:36 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
