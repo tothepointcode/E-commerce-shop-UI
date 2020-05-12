5ef568646ed34f8f21c98a297d3e8fb94acaa6b9
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-05-12 22:53:18 +0000

    Feat: Horizontal category list setup.

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,13 +1,33 @@
-import * as React from "react";
+import React, {useState} from "react";
 
 // UI
 import { View, Text, TextInput, ScrollView } from "react-native";
-import { Feather} from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 // Custom styles
 import { basic, colors } from "../styles";
+import Categories from "./Categories";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Home = () => {
+    const [active, setActive] = useState(1);
+  const categories = [
+    {
+      name: "Interiors",
+    },
+    {
+      name: "Furniture",
+    },
+    {
+      name: "Moods",
+    },
+    {
+      name: "Creators",
+    },
+    {
+      name: "Home",
+    },
+  ];
   return (
     <View style={basic.body}>
       <View style={basic.inputSection}>
@@ -18,15 +38,15 @@ const Home = () => {
         />
         <Feather name="search" style={basic.icon} />
       </View>
-      <ScrollView style={basic.inputSection}>
-        <TextInput
-          placeholder="Search..."
-          placeholderTextColor={colors.placeholder}
-          style={basic.input}
-        />
-        <Feather name="search" style={basic.icon} />
+      <ScrollView horizontal={true} style={basic.inputSection}>
+        {categories.map((category, index) => {
+          return (
+            <TouchableOpacity style={basic.category} onPress={()=> setActive(index)}>
+              <Text style={[basic.categoryText, (active === index) && basic.activeCategory]}>{category.name}</Text>
+            </TouchableOpacity>
+          );
+        })}
       </ScrollView>
-
     </View>
   );
 };
--- styles.js
+++ styles.js
@@ -1,35 +1,50 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const colors = {
-    primary: "#fff",
-    secondary: "#f0f0f0",
-    alt: "#1A202C",
-    placeholder: "#CBD5E0"
-}
+  primary: "#fff",
+  secondary: "#f0f0f0",
+  alt: "#1A202C",
+  placeholder: "#CBD5E0",
+  black: "#0f0f0f",
+};
 
 export const basic = StyleSheet.create({
-    body: {
-        flex: 1,
-        padding: 30,
-        backgroundColor: colors.primary
-    },
-    inputSection: {
-        position: "relative",
-        marginBottom: 25
-    },
-    input: {
-        backgroundColor: colors.secondary,
-        height: 70,
-        borderRadius: 20,
-        paddingLeft: 70,
-        fontSize: 17,
-        color: colors.alt
-    }, 
-    icon: {
-        fontSize: 25,
-        position: 'absolute',
-        top: 22.5,
-        left: 22.5,
-        color: colors.placeholder
-    }
-})
+  body: {
+    flex: 1,
+    padding: 30,
+    backgroundColor: colors.primary,
+  },
+  inputSection: {
+    position: "relative",
+    marginBottom: 25,
+  },
+  input: {
+    backgroundColor: colors.secondary,
+    height: 70,
+    borderRadius: 20,
+    paddingLeft: 70,
+    fontSize: 17,
+    color: colors.alt,
+  },
+  icon: {
+    fontSize: 25,
+    position: "absolute",
+    top: 22.5,
+    left: 22.5,
+    color: colors.placeholder,
+  },
+  category: {
+    paddingRight: 25,
+    paddingVertical: 5,
+  },
+  categoryText: {
+    color: colors.placeholder,
+    fontWeight: "600",
+  },
+  activeCategory: {
+    color: colors.alt,
+    borderBottomWidth: 3,
+    paddingBottom: 5,
+    borderBottomWidth: 3,
+  },
+});
